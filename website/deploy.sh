31844b4c69160fa44fd74940ebd91f439894547c
Ender Oksam <Enderoksam18@gmail.com>
2026-06-14 15:40:50 -0500

    changing domain

--- website/deploy.sh
+++ website/deploy.sh
@@ -3,4 +3,4 @@ set -e
 cd "$(dirname "$0")"
 python ../scripts/build_web.py
 npm run build
-npx wrangler pages deploy .svelte-kit/cloudflare --project-name=gsgw
+npx wrangler pages deploy .svelte-kit/cloudflare --project-name=ireum
