2a35de44742b3cbc5ce69d54da50f963ba7f777a
Ender Oksam <Enderoksam18@gmail.com>
2026-07-18 23:00:40 -0500

    adapter static

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-cloudflare";
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,7 @@ const config = {
     vitePreprocess(),
   ],
   kit: {
-    adapter: adapter(),
+    adapter: adapter({ strict: false, fallback: "200.html" }),
 
     prerender: {
       concurrency: 5,
